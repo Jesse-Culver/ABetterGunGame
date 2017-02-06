7edd0b881fc10b8378fbee26435e44e02bd08b54
jesseculver <jesseculver92@gmail.com>
2017-02-06 14:23:07 -0600

    weapontable handles things pertaining to the weapontable

---
+++ gamemode/weapontable.lua
@@ -0,0 +1,52 @@
+--Contains functions related to the weapon table
+
+local weapon_table = {
+};
+
+--Checks if the proper files exist then fills the weapon table
+function CreateWeaponTable()
+  if file.Exists("abgg","DATA") then
+    print("abgg folder exists")
+  else
+    print("abgg folder does not exist, creating it")
+    file.CreateDir("abgg")
+  end
+  if file.Exists("weapons.txt","DATA/abgg") then
+    print("Reading in weapons.txt file")
+    FillWeaponTable()
+  else
+    print("Creating weapons file")
+    local tempWeaponTable = [[
+weapon_abgg_hl2pistol
+weapon_abgg_hl2smg1
+weapon_abgg_hl2357]]
+    file.Write("abgg/weapons.txt", tempWeaponTable)
+    FillWeaponTable()
+  end
+end
+
+function FillWeaponTable()
+  local weaponFile = file.Read("abgg/weapons.txt")
+  weaponFile  = string.Explode("\n", weaponFile)
+  for k, v in pairs(weaponFile) do
+    table.insert(weapon_table,k,v)
+  end
+end
+
+function GetMaxLevel()
+  return #weapon_table
+end
+
+function RandomizeWeaponOrder()
+  PrintTable(weapon_table)
+  local upperBound = #weapon_table
+  local tempTable = {
+};
+  for i=1,upperBound do
+    local tempRand = math.random(#weapon_table)
+    table.insert(tempTable,weapon_table[tempRand])
+    table.remove(weapon_table,tempRand)
+  end
+  table.CopyFromTo(tempTable,weapon_table)
+  PrintTable(weapon_table)
+end
